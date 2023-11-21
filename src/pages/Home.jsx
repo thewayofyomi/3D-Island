ac6bdcf837e69ea00361ee9198804b06f89d449c
Sean Humphreys <130619362+thewayofyomi@users.noreply.github.com>
2023-11-20 20:42:02 -0500

    created homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import {Suspense} from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader"
+
+{/* <div className="absolute left-0 right-0 z-10 flex items-center justify-center top-28">
+  POPUP
+</div> */}
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="relative w-full h-screen">
+      <Canvas className="w-full h-screen bg-transparent" camera={{ near: 0.1, far: 1000 }}>
+        <Suspense fallback={ <Loader /> }>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
